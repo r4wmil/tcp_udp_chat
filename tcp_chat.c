56abd2f9687f55475ece9f269c6f2bdafac7545d
Ramil Khasanshin <khasanshin.ramil@list.ru>
2026-08-23 19:22:35 +0300

    feat(task4-5): working chat client in C

--- tcp_chat.c
+++ tcp_chat.c
@@ -3,21 +3,70 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <poll.h>
+
+#define MAX_BUF 4096
 
 int main() {
 	int sock = socket(AF_INET, SOCK_STREAM, 0);
-	struct sockaddr_in serv_addr = {AF_INET, htons(7777)};
+
+	struct sockaddr_in serv_addr = {
+		.sin_family = AF_INET,
+		.sin_port = htons(7777)
+	};
 	inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr);
 
-	connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+	int c = -1;
+	for (;;) {
+				c = connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+				if (c >= 0) break;
+		perror("connect");
+		sleep(2);
+	}
 
-	char* msg = "hello\n";
-	write(sock, msg, strlen(msg));
+	printf("connected.\n");
 
-	char buf[1024] = {0};
-	read(sock, buf, 1024);
+	char buf[MAX_BUF];
 
-	printf("%s\n", buf);
+	struct pollfd fds[2] = {
+		{ STDIN_FILENO, POLLIN, 0 },
+		{ sock,		 POLLIN, 0 }
+	};
 
+	for (;;) {
+		if (poll(fds, 2, -1) < 0) {
+			perror("poll");
+			break;
+		}
+
+		// stdin -> server
+		if (fds[0].revents & POLLIN) {
+			int n = read(STDIN_FILENO, buf, sizeof(buf));
+			if (n <= 0) break;
+
+			send(sock, buf, n, 0);
+		}
+
+		// server -> stdout
+		if (fds[1].revents & POLLIN) {
+			int n = recv(sock, buf, sizeof(buf) - 1, 0);
+
+			if (n == 0) {
+				printf("\ndisconnected by server.\n");
+				break;
+			}
+
+			if (n < 0) {
+				perror("recv");
+				break;
+			}
+
+			buf[n] = '\0';
+			printf("%s", buf);
+			fflush(stdout);
+		}
+	}
+
+	close(sock);
 	return 0;
 }
