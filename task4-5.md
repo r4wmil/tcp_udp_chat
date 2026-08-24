6f0089b7f5ad659db3d75c78d8f3dcc8802086b6
Ramil Khsasnshin <khasanshin.ramil@list.ru>
2026-08-24 23:37:33 +0300

    feat(task4-5): TCP chat server

--- task4-5.md
+++ task4-5.md
@@ -2,5 +2,8 @@
 Build & run:
 ```
 gcc tcp_chat.c -o tcp_chat
-./tcp_chat
+# server
+./tcp_chat s - 7777
+# client
+./tcp_chat c 127.0.0.1 7777
 ```
--- tcp_chat.c
+++ tcp_chat.c
@@ -31,11 +31,9 @@ void parse_args(int argc, char** argv) {
 	return;
 }
 
-int main(int argc, char** argv) {
+// --- Client ---
 
-	parse_args(argc, argv);
-	if (!args.ok) { return 1; }
-
+void client() {
 	int sock = socket(AF_INET, SOCK_STREAM, 0);
 
 	struct sockaddr_in serv_addr = {
@@ -96,5 +94,92 @@ int main(int argc, char** argv) {
 	}
 
 	close(sock);
+}
+
+// --- Server ---
+
+void server(void) {
+	int sock = socket(AF_INET, SOCK_STREAM, 0);
+
+	struct sockaddr_in serv_addr = {
+		.sin_family = AF_INET,
+		.sin_port = htons(args.port),
+		.sin_addr.s_addr = INADDR_ANY
+	};
+
+	bind(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+	listen(sock, 1);
+
+	printf("waiting for connection...\n");
+
+	int client = accept(sock, NULL, NULL);
+	if (client < 0) {
+		perror("accept");
+		close(sock);
+		return;
+	}
+
+	printf("connected.\n");
+
+	char buf[MAX_BUF];
+
+	struct pollfd fds[2] = {
+		{ STDIN_FILENO, POLLIN, 0 },
+		{ client,       POLLIN, 0 }
+	};
+
+	for (;;) {
+		if (poll(fds, 2, -1) < 0) {
+			perror("poll");
+			break;
+		}
+
+		// stdin -> client
+		if (fds[0].revents & POLLIN) {
+			int n = read(STDIN_FILENO, buf, sizeof(buf));
+			if (n <= 0) break;
+
+			send(client, buf, n, 0);
+		}
+
+		// client -> stdout
+		if (fds[1].revents & POLLIN) {
+			int n = recv(client, buf, sizeof(buf) - 1, 0);
+
+			if (n == 0) {
+				printf("\ndisconnected by client.\n");
+				break;
+			}
+
+			if (n < 0) {
+				perror("recv");
+				break;
+			}
+
+			buf[n] = '\0';
+			printf("%s", buf);
+			fflush(stdout);
+		}
+	}
+
+	close(client);
+	close(sock);
+}
+
+// --- Main ---
+
+int main(int argc, char** argv) {
+
+	parse_args(argc, argv);
+	if (!args.ok) { return 1; }
+
+	if (args.is_serv) {
+		printf("server starting...\n");
+		server();
+	} else {
+		printf("client starting...\n");
+		client();
+	}
+
 	return 0;
 }
